8e11ea95a55bb4d255509d9c40c5b534aa73e21c
Eduardo de Moura Rodrigues <eduardom.rodrigues@gmail.com>
2016-10-29 07:48:55 -0200

    Criação da estrutura do projeto #2

--- meuprontuario/src/main/java/br/com/meuprontuario/ws/UsuarioResource.java
+++ meuprontuario/src/main/java/br/com/meuprontuario/ws/UsuarioResource.java
@@ -1,8 +1,14 @@
 package br.com.meuprontuario.ws;
 
+import java.sql.SQLException;
+
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceContextType;
+import javax.sql.DataSource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -10,20 +16,21 @@ import javax.ws.rs.Produces;
 import br.com.meuprontuario.modelo.Usuario;
 
 @Path("/v1/usuarios")
+@Stateless
 public class UsuarioResource {
 
-	@PersistenceContext(unitName = "meuProntuarioPU", type = PersistenceContextType.EXTENDED)
+	@PersistenceContext(unitName = "meuProntuarioPU")
 	private EntityManager entityManager;
 
 	@GET
 	@Produces("text/plain")
-	public String helloWorld() {
+	public String helloWorld() throws NamingException, SQLException {
 
-		
 		Usuario usuario = new Usuario();
+		usuario.setNome("Eduardo2");
+		
 		entityManager.persist(usuario);
-		
-		
+
 		return "oi: " + entityManager;
 
 	}
